2df5ccfb4e3509ace84aa295b5d6449d56cda0e5
Verena Blaschke <verena.blaschke@gmx.de>
2020-03-10 21:50:41 +0100

    More NE features

--- utils/add_named_entity_features.py
+++ utils/add_named_entity_features.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import spacy
 import re
 
@@ -42,6 +43,38 @@ def annotate_file_ner(nlp, features, in_file):
                 print(idx, text, results)
 
 
+def search_all(nlp, text):
+    results = set()
+    doc = nlp(text)
+    for ent in doc.ents:
+        results.add(ent.label_)
+    # Sometimes multi-word units containing a country name or nationality are
+    # classified as 'ORG' instead of 'GPE'/'NORP'
+    # -> Check individual tokens (out of context)
+    if len(doc) > 1:
+        for tok in doc:
+            for ent in nlp(tok.text).ents:
+                results.add(ent.label_)
+    return results
+
+
+def match_all_ne(nlp, in_file):
+    with open(in_file, encoding='utf8') as f:
+        lines = f.readlines()
+
+    matches = []
+
+    for idx, line in enumerate(lines[1:]):
+        text = line.split('\t')[4]
+        results = search_all(nlp, text)
+        matches.extend(results)
+        if idx % 250 == 0:
+            print(idx, text, results)
+
+    print(sorted(dict(Counter(matches)).items(), key=lambda item: item[1],
+                 reverse=True))
+
+
 def find_america(text):
     if 'america' not in text:
         return '0'
@@ -81,8 +114,18 @@ def annotate_file_america_simple(in_file):
 
 
 nlp = spacy.load('en_core_web_sm')
-features = ['NORP',  # Nationalities or religious or political groups
-            'GPE']   # Geopolitical entities: Countries, cities, states
+# match_all_ne(nlp, '../data/tc-train.tsv')
+# [('ORG', 729), ('NORP', 573), ('GPE', 522), ('PERSON', 507),
+#  ('CARDINAL', 287), ('DATE', 163), ('ORDINAL', 69), ('LOC', 50),
+#  ('WORK_OF_ART', 38), ('EVENT', 25), ('LAW', 19), ('TIME', 15),
+#  ('MONEY', 13), ('PERCENT', 12), ('FAC', 11), ('PRODUCT', 7),
+#  ('LANGUAGE', 5), ('QUANTITY', 3)]
+
+features = ['ORG',
+            'NORP',  # Nationalities or religious or political groups
+            'GPE',   # Geopolitical entities: Countries, cities, states
+            'PERSON', 'CARDINAL', 'DATE'
+            ]
 annotate_file_ner(nlp, features, '../data/tc-train.tsv')
 annotate_file_ner(nlp, features, '../data/tc-dev.tsv')
 annotate_file_ner(nlp, features, '../data/tc-test.tsv')
